8c0da4cd36a89c000b539b5e19363f5f379af70e
phonsudanan <phonsuda-nan@hotmail.com>
2024-04-24 14:19:46 +0700

    [Created]: เรียน Python วันที่ 3

---
+++ Day_3/Assignment_2.py
@@ -0,0 +1,9 @@
+# Assignment 2: โปรแกรมเปรียบเทียบตัวเลข
+
+a = int(input('Number 1 : '))
+b = int(input('Number 2 : '))
+
+if a > b:
+    print(a, ' มากกว่า ' ,b)
+else:
+    print(a, ' น้อยกว่า ' ,b)
